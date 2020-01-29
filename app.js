2f29bce19fed1e078c734b228467454e06d10c12
Tim-Shaffer <tcshaf24@ptd.net>
2020-01-28 22:08:47 -0500

    made changes to get the articles to populate on the page

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 
 $("#searchSubmit").on("click", function(){
+    console.log("Search Button Clicked");
 
-let articleQuery = $("#searchTerm");
+let articleQuery = $("#searchTerm").val();
+    console.log(articleQuery);
 
-let numRecords= $("#numOfRecords").attr("value");
-let startYear = $("#startYear");
-let endYear = $("#endYear");
+let numRecords= $("#numOfRecords").val();
+    console.log(numRecords);
+// let startYear = $("#startYear");
+// let endYear = $("#endYear");
 
-let queryurl="https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+articleQuery+"&api-key=BLujw6QB4o3UHSrHd4yd5wtzQFqjWOyo";  //whatever is in var articleQuery is what get from ui team
+let queryurl="https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ articleQuery +"&api-key=BLujw6QB4o3UHSrHd4yd5wtzQFqjWOyo";  
 
-
-// q=obama&facet_fields=source&facet=true&begin_date=20120101&end_date=20121231
-
-// headline 
-
-
-
-        $.ajax({
-            url: queryurl, 
-            method: "get"  
-            }).then(function(response){
-                console.log(response)
+    $.ajax({
+        url: queryurl, 
+        method: "get"  
+    }).then(function(response){
+ 
             for(i = 0; i < numRecords; i++){
-                console.log(response.docs[i].snippet);
-            }        
-        }   
 
-                    )
-                })
+                var newArticle = $("<h2>");
+                newArticle.text(response.response.docs[i].snippet);
+                var author = $("<p>");
+                author.text(response.response.docs[i].byline.original);
+                $("#articlesResults").append(newArticle);
+                $("#articlesResults").append(author);
+            }        
+        })
+})
 
 
     
